0b435aa0009a525e72fb65257a05660058e1d935
Miguel Alonso <malbonm07@gmail.com>
2019-06-06 16:42:13 -0500

    Updates

---
+++ _nuxt/bc5ffd471f5a12e95941.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{296:function(n,o,l){"use strict";l.r(o);var e=l(47),component=Object(e.a)({},void 0,void 0,!1,null,null,null);o.default=component.exports}}]);
